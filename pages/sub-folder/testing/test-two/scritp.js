d28649e3f92471ab67836f0fca3c26c74708460b
shortpupper <78228290+shortpupper@users.noreply.github.com>
2021-11-01 16:49:20 -0500

    new

---
+++ pages/sub-folder/testing/test-two/scritp.js
@@ -0,0 +1,9 @@
+element.attribute="new value"
+document.getElementById("example").src="otherPicture.jpg";
+myButton = document.getElementById("myButton"); //searches for and detects the input element from the 'myButton' id
+myButton.value = "I'm a changed button"; //changes the value
+myButton.type = "text"; //changes the input type from 'button' to 'text'.
+var e = document.getElementById("div2"); //Get the element
+e.setAttribute("id", "div3"); //Change id to div3
+var e = document.createElement('div'); //Make a div element (we also could have made it with HTML)
+e.createAttribute("id", "myDiv"); //Set the id to "myDiv"
